64bdaa840dff50a26fc9cf353bcdc64c23a71447
andcliu <andcliu@gmail.com>
2017-08-08 14:26:39 -0700

    talbe updated

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,14 @@ class StaticPagesController < ApplicationController
   end
 
   def order_page
+  	@all_prod = Product.all
+  	@steamed = Category.first.products
+  	@bun = Category.first.products
+  	@baked = Category.first.products
+  	@rice = Category.first.products
+  	@fried = Category.first.products
+  	@dessert = Category.first.products
+  	@bbq = Category.first.products
+  	@chefs = Category.first.products
   end
 end
